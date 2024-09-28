3063ffb013d5402969a08ee29a037f973e51c0cd
rtr8962 <rtr8962@rit.edu>
2024-09-28 16:01:11 -0400

    rename searchNeed to searchNeeds in NeedDAO

--- ufund-api/src/main/java/com/ufund/api/ufundapi/persistence/NeedDAO.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/persistence/NeedDAO.java
@@ -85,6 +85,6 @@ public interface NeedDAO {
      * 
      * @throws IOException if underlying storage cannot be accessed
      */
-    Need[] searchNeed(String search) throws IOException;
+    Need[] searchNeeds(String search) throws IOException;
 
 }
