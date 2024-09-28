d16070d5317a5f67c5cdf6213d1a325fda6616b9
rtr8962 <rtr8962@rit.edu>
2024-09-28 15:23:04 -0400

    Revert "Changed getNeed id param from String to int"
    This reverts commit b7b58f55471e0ee65ea1b9705e14e2129f33ebd1.

--- ufund-api/src/main/java/com/ufund/api/ufundapi/persistence/NeedDAO.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/persistence/NeedDAO.java
@@ -37,7 +37,7 @@ public interface NeedDAO {
      * 
      * @throws IOException if an issue with underlying storage
      */
-    Need getNeed(int id) throws IOException;
+    Need getNeed(String id) throws IOException;
 
     /**
      * Creates and saves a {@linkplain Need Need}
