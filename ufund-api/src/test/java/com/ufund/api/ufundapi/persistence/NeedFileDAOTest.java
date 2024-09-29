953f48c654ae336686aa11ff3a7bc8645c6c5899
kjv7359 <kjv7359@rit.edu>
2024-09-29 10:16:24 -0400

    add FileDAO getNeed test

---
+++ ufund-api/src/test/java/com/ufund/api/ufundapi/persistence/NeedFileDAOTest.java
@@ -0,0 +1,57 @@
+package com.ufund.api.ufundapi.persistence;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ufund.api.ufundapi.model.Need;
+
+/**
+ * Test the Need File DAO class
+ * 
+ * @author SWEN Faculty
+ */
+@Tag("Persistence-tier")
+public class NeedFileDAOTest {
+    NeedFileDAO needFileDAO;
+    Need[] testNeeds;
+    ObjectMapper mockObjectMapper;
+
+    /**
+     * Before each test, we will create and inject a Mock Object Mapper to
+     * isolate the tests from the underlying file
+     * @throws IOException
+     */
+    @BeforeEach
+    public void setupNeedFileDAO() throws IOException {
+        mockObjectMapper = mock(ObjectMapper.class);
+        testNeeds = new Need[3];
+        testNeeds[0] = new Need("MOCKID-0","NeedA1", "Description1", 0);
+        testNeeds[1] = new Need("MOCKID-1","NeedB1", "Description2", 2.9);
+        testNeeds[2] = new Need("MOCKID-2","NeedB2", "Description3", 1.3);
+
+        // When the object mapper is supposed to read from the file
+        // the mock object mapper will return the hero array above
+        when(mockObjectMapper
+            .readValue(new File("doesnt_matter.txt"),Need[].class))
+                .thenReturn(testNeeds);
+        needFileDAO = new NeedFileDAO("doesnt_matter.txt",mockObjectMapper);
+    }
+
+    @Test
+    public void testGetNeed() throws IOException {
+        // Invoke
+        Need need = needFileDAO.getNeed("MOCKID-0");
+
+        // Analyze
+        assertEquals(need, testNeeds[0]);
+    }
+}
