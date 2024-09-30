cd548ce2ddb4ac2787c086e10d3601776913587a
kjv7359 <kjv7359@rit.edu>
2024-09-30 16:09:50 -0400

    complete script

--- ufund-api/demos/test-getneed.sh
+++ ufund-api/demos/test-getneed.sh
@@ -18,6 +18,9 @@ create_need() {
 echo "get all needs when none exist"
 curl "http://localhost:8080/needs"
 
+echo -e "\n\nPress enter to continue..."
+read
+
 echo -e "\n\ncreate first need:"
 id1=$(create_need <<'EOF'
 {
@@ -29,6 +32,9 @@ id1=$(create_need <<'EOF'
 }
 EOF
 )
+
+echo -e "\n\nPress enter to continue..."
+read
 
 echo -e "\n\ncreate second need:"
 id2=$(create_need <<'EOF'
@@ -42,6 +48,9 @@ id2=$(create_need <<'EOF'
 EOF
 )
 
+echo -e "\n\nPress enter to continue..."
+read
+
 echo -e "\n\ncreate third need:"
 id3=$(create_need <<'EOF' >/dev/null
 {
@@ -54,35 +63,74 @@ id3=$(create_need <<'EOF' >/dev/null
 EOF
 )
 
-echo -e "\n\nget one need:"
+echo -e "\n\nPress enter to continue..."
+read
+
+echo -e "\n\nget one need, NeedA1:"
 curl "http://localhost:8080/needs/$id1"
 
-echo -e "\n\nget a need that doesn't exist:"
+echo -e "\n\nPress enter to continue..."
+read
+
+echo -e "\n\nget a need that doesn't exist, need with id \"notFound\":"
 ! curl "http://localhost:8080/needs/notFound"
+
+echo -e "\n\nPress enter to continue..."
+read
 
 echo -e "\n\nget all needs"
 curl "http://localhost:8080/needs"
 
-echo -e "\n\nsearch for needs that exist"
+echo -e "\n\nPress enter to continue..."
+read
+
+echo -e "\n\nsearch for needs that exist, searching \"NeedA\""
 curl "http://localhost:8080/needs/?search=NeedA"
 
-echo -e "\n\nsearch for a need that doesn't exist"
+echo -e "\n\nPress enter to continue..."
+read
+
+echo -e "\n\nsearch for a need that doesn't exist, searching \"NeedC\""
 curl "http://localhost:8080/needs/?search=NeedC"
 
-echo -e "\n\ndelete a need"
+echo -e "\n\nPress enter to continue..."
+read
+
+echo -e "\n\nall needs before deletion"
+curl "http://localhost:8080/needs"
+
+echo -e "\n\ndelete a need, \"NeedA1\""
 curl -X DELETE "http://localhost:8080/needs/$id1"
 
-echo -e "\n\nget all needs again. \"NeedA1\" is gone."
+echo -e "\n\nall needs after deletion. \"NeedA1\" is gone."
 curl "http://localhost:8080/needs"
+
+echo -e "\n\nPress enter to continue..."
+read
 
 echo -e "\n\ndelete need 'c2' with incorrect id"
 ! curl -i -X DELETE 'http://localhost:8080/needs/c2'
 
-echo -e "\n\nget all needs again"
+echo -e "\n\nall needs again. Same as before"
+curl "http://localhost:8080/needs"
+
+echo -e "\n\nPress enter to continue..."
+read
+
+echo -e "\n\nall needs before \"NeedA2\" is updated"
 curl "http://localhost:8080/needs"
 
 echo -e "\n\nupdate need \"NeedA2\" to \"UpdatedNeed\""
-curl.exe -i -X PUT -H 'Content-Type:application/json' 'http://localhost:8080/needs' -d '{"id": $id2,"name": "UpdatedNeed"}'
+curl.exe -i -X PUT -H "Content-Type:application/json" "http://localhost:8080/needs" -d "{\"id\": \"$id2\",\"name\": \"UpdatedNeed\"}"
+
+echo -e "\n\nall needs after"
+curl "http://localhost:8080/needs"
+
+echo -e "\n\nPress enter to continue..."
+read
 
 echo -e "\n\nupdate need \"NeedA2\" with incorrect id"
-! curl.exe -i -X PUT -H 'Content-Type:application/json' 'http://localhost:8080/needs' -d '{"id": "notId","name": "IncorrectNeed"}'
+! curl.exe -i -X PUT -H "Content-Type:application/json" "http://localhost:8080/needs" -d "{\"id\": \"notId\",\"name\": \"IncorrectNeed\"}"
+
+echo -e "\n\nall needs after. Nothing has changed."
+curl "http://localhost:8080/needs"
