13a08ba5fc1b8f85078480d472d3ed2b8d3c2e4f
mbk9864 <mbk9864@rit.edu>
2024-10-09 16:30:26 -0400

    BasketNeed as Json

--- ufund-api/src/main/java/com/ufund/api/ufundapi/model/Basket.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/model/Basket.java
@@ -1,8 +1,20 @@
 package com.ufund.api.ufundapi.model;
 import java.util.ArrayList;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Basket {
+    @JsonProperty
     private ArrayList<BasketNeed> donationNeeds;
+    @JsonProperty
     private ArrayList<BasketNeed> adoptionNeeds;
+    @JsonProperty
     private ArrayList<BasketNeed> volunteerNeeds;
+
+    public Basket(){
+
+        donationNeeds = new ArrayList<>();
+        adoptionNeeds = new ArrayList<>();
+        volunteerNeeds = new ArrayList<>();
+    }
 }
