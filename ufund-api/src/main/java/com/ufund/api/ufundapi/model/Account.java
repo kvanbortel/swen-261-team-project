814ec5a30f622a0ea4c5541c5a5bac39881005b2
mbk9864 <mbk9864@rit.edu>
2024-10-09 15:37:08 -0400

    Creation of basket and basketneed

--- ufund-api/src/main/java/com/ufund/api/ufundapi/model/Account.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/model/Account.java
@@ -1,6 +1,5 @@
 package com.ufund.api.ufundapi.model;
 
-import java.util.ArrayList;
 import java.util.logging.Logger;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -10,19 +9,19 @@ public class Account {
     private static final Logger LOG = Logger.getLogger(Account.class.getName());
 
     @JsonProperty String name;
-    @JsonProperty ArrayList<Need> needs;
+    @JsonProperty Basket basket;
 
     public Account(@JsonProperty("name") String name){
         this.name = name;
-        this.needs = new ArrayList<Need>();
+        this.basket = new Basket();
     }
 
     public String getName(){
         return this.name;
     }
 
-    public ArrayList<Need> getNeeds(){
-        return this.needs;
+    public Basket getBasket(){
+        return this.basket;
     }
 
     public String toString(){
---
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/model/Basket.java
@@ -0,0 +1,8 @@
+package com.ufund.api.ufundapi.model;
+import java.util.ArrayList;
+
+public class Basket {
+    private ArrayList<BasketNeed> donationNeeds;
+    private ArrayList<BasketNeed> adoptionNeeds;
+    private ArrayList<BasketNeed> volunteerNeeds;
+}
