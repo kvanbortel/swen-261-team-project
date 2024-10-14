0416ea72365f0f6f2fc377e04c0dd719f4d08b66
Ryan <ryan@rtrichter.com>
2024-10-13 20:17:26 -0400

    stubbed methods for Basket

--- ufund-api/src/main/java/com/ufund/api/ufundapi/model/Basket.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/model/Basket.java
@@ -33,4 +33,39 @@ public class Basket {
     public ArrayList<BasketNeed> getNeeds() {
         return needs;
     }
+
+    // stubbing methods (remove this comment when all methods are implemented)
+
+
+    /**
+     * Gets a BasketNeed given its id if it is in the basket, otherwise returns null
+     * @param needId
+     * @return
+     */
+    public BasketNeed getBasketNeed(String needId) { return new BasketNeed("FAKEID", 0); }
+
+    /**
+     * Determines if the basket contains a needId
+     * 
+     * @param needId needId to search for
+     * 
+     * @return whether or not the needId is found
+     */
+    public boolean hasNeed(String needId) { return false; }
+
+    /**
+     * Adds a need to the basket. If that need has already been added, 
+     * increases the quantity by 1
+     * 
+     * @param needId the needId to add to the basket
+     */
+    public void addNeed(String needId) {}
+
+    /**
+     * Removes a need form the basket. If the quantity of that need is 
+     * more than 1, just decrements the quantity field
+     * 
+     * @param needId the neewdId to remove from the basket
+     */
+    public void removeNeed(String needId) {}
 }
