0f6865c4988514823cffb5c6f035a587a58bdf7b
jonah <jonahwitte10@gmail.com>
2024-10-18 18:49:30 -0400

    added form controlled to text field for quantity

--- ufund-ui/angular/src/app/basket-need/basket-need.component.ts
+++ ufund-ui/angular/src/app/basket-need/basket-need.component.ts
@@ -1,29 +1,72 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
 import { BasketNeed } from '../BasketNeed';
 import { AuthService } from '../storage/auth.service';
 import { Need } from '../Need';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-basket-need',
   templateUrl: './basket-need.component.html',
-  styleUrl: './basket-need.component.css'
+  styleUrl: './basket-need.component.css',
 })
 export class BasketNeedComponent {
-  @Input() need?: BasketNeed;
-
-  emptyNeed: Need = {id: "",
-    name: "",
-    description: "",
+  emptyNeed: Need = {
+    id: '',
+    name: '',
+    description: '',
     fulfillmentStatus: false,
     demandRating: 1.0,
     cost: 1.0,
-    quantity: 1 }
+    quantity: 1,
+  };
 
-  getNeed(){
-    return this.need ?  this.need.need : this.emptyNeed
+  emptyBNeed: BasketNeed = { quantity: 1, need: this.emptyNeed };
+
+  @Input() need: BasketNeed;
+
+  ngOnChanges(changes: SimpleChanges) {
+    this.need = changes['need'].currentValue; // fetch the current value
+    this.quantityForm = this.formBuilder.group({
+      quantity: [
+        this.need.quantity,
+        [Validators.min(1), Validators.max(this.getNeed().quantity)],
+      ],
+    });
   }
 
-  constructor(public authService: AuthService) {}
+  getNeed() {
+    return this.need ? this.need.need : this.emptyNeed;
+  }
+
+  quantityForm: FormGroup; // Instantiating our form
+
+  constructor(
+    public authService: AuthService,
+    public formBuilder: FormBuilder
+  ) {
+    this.need = this.emptyBNeed;
+    this.quantityForm = formBuilder.group({
+      quantity: [
+        '',
+        [Validators.min(1), Validators.max(this.getNeed().quantity)],
+      ],
+    });
+  }
+
+  addQuantity() {
+    this.authService.addToBasket()
+  }
 
   retrieveNeed() {
     return this.need ? Object.keys(this.need) : [];
@@ -35,7 +78,7 @@ export class BasketNeedComponent {
     this.dataFromChild.emit(this.need);
   }
 
-  sendUpdateNeeds(){
+  sendUpdateNeeds() {
     // tell the parent to update its needs
     this.dataFromChild.emit();
   }
