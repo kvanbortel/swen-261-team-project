5efd4b3d94a447409bd981f13429d4888c6f5ddc
jonah <jonahwitte10@gmail.com>
2024-10-21 14:50:53 -0400

    add checkout confirmation

--- ufund-ui/angular/src/app/basket-list/basket-list.component.ts
+++ ufund-ui/angular/src/app/basket-list/basket-list.component.ts
@@ -4,56 +4,82 @@ import { AuthService } from '../storage/auth.service';
 import { async, BehaviorSubject } from 'rxjs';
 import { BasketNeed } from '../BasketNeed';
 import { Router } from '@angular/router';
+import { ConfirmationDialogComponent } from '../confirmation-dialog/confirmation-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-basket-list',
   templateUrl: './basket-list.component.html',
-  styleUrl: './basket-list.component.css'
+  styleUrl: './basket-list.component.css',
+  providers: [CurrencyPipe]
 })
 export class BasketListComponent {
   basketNeeds$ = new BehaviorSubject<BasketNeed[]>([]);
 
-  constructor(public authService: AuthService, private router: Router) {}
-
+  constructor(
+    public authService: AuthService,
+    public dialog: MatDialog,
+    private router: Router,
+    private currencyPipe: CurrencyPipe
+  ) {}
 
   ngOnInit(): void {
     this.authService.getBasketNeeds().subscribe({
-      next: (response) => { this.basketNeeds$.next(response); console.log(response)}
+      next: (response) => {
+        this.basketNeeds$.next(response);
+        console.log(response);
+      },
     });
   }
 
   onNeedDeleted(needToDelete: BasketNeed) {
     const currentNeeds = this.basketNeeds$.getValue();
 
-    const updatedNeeds = currentNeeds.filter(need => need.need.id !== needToDelete.need.id);
+    const updatedNeeds = currentNeeds.filter(
+      (need) => need.need.id !== needToDelete.need.id
+    );
 
     this.basketNeeds$.next(updatedNeeds);
   }
 
   getTotalQuantity(): number {
     const currentNeeds = this.basketNeeds$.getValue();
-    console.log("gotten")
+    console.log('gotten');
     return currentNeeds.reduce((total, need) => total + need.quantity, 0);
-  }
-
-  checkout(): void{
-    if(this.getTotalQuantity() > 0){
-      this.authService.checkoutBasket();
-
-      const totalQuantity = this.getTotalQuantity();
-      const totalCost = this.getTotalCost();
-  
-      this.router.navigate(['/post-checkout'], {
-        state: {
-          amount: totalQuantity,
-          cost: totalCost
-        }
-      });
-    }
   }
 
   getTotalCost(): number {
     const currentNeeds = this.basketNeeds$.getValue();
-    return currentNeeds.reduce((total, need) => total + (need.need.cost) * need.quantity, 0);
+    return currentNeeds.reduce(
+      (total, need) => total + need.need.cost * need.quantity,
+      0
+    );
+  }
+
+  checkout(): void {
+    if (this.getTotalQuantity() > 0) {
+      const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
+        disableClose: false,
+      });
+      dialogRef.componentInstance.confirmMessage = `Are you sure you want to fund ${this.getTotalQuantity()} needs for ${this.currencyPipe.transform(this.getTotalCost(), 'USD', 'symbol', '1.2-2')}?`;
+
+      dialogRef.afterClosed().subscribe((result) => {
+        console.log('The dialog was closed');
+        if (result) {
+          this.authService.checkoutBasket();
+
+          const totalQuantity = this.getTotalQuantity();
+          const totalCost = this.getTotalCost();
+
+          this.router.navigate(['/post-checkout'], {
+            state: {
+              amount: totalQuantity,
+              cost: totalCost,
+            },
+          });
+        }
+      });
+    }
   }
 }
