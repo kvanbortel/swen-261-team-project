bb6f6bcd5a55e42d031b2be56c6206b509b5dca6
Max Klot <maxbklot@gmail.com>
2024-10-28 18:55:29 -0400

    Username Regex Checking

--- ufund-ui/angular/src/app/login/login.component.ts
+++ ufund-ui/angular/src/app/login/login.component.ts
@@ -3,6 +3,7 @@ import { AuthService } from '../storage/auth.service';
 import { catchError, Observable, of, tap } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-login',
@@ -20,11 +21,25 @@ export class LoginComponent {
   constructor(
     public authService: AuthService,
     private http: HttpClient,
-    private router: Router
+    private router: Router,
+    public messageLoginService: MessageService
   ) {}
 
   login(name: string) {
+    this.messageLoginService.clear()
     if(name == '' || name == null){
+      return
+    }
+    if(!this.onlyLettersAndNumbers(name)){
+      this.messageLoginService.add("Only usernames containing alphanumeric characters allowed.", false)
+      return
+    }
+    if(name.length > 13){
+      this.messageLoginService.add("Only usernames of length 13 or less allowed.", false)
+      return
+    }
+    if(!this.isAlpha(name.substring(0, 1))){
+      this.messageLoginService.add("Only usernames starting with a letter allowed.", false)
       return
     }
     this.authService.setName(name);
@@ -43,7 +58,16 @@ export class LoginComponent {
   }
 
   ngOnInit(): void {
+    this.messageLoginService.add('', false);
     this.authService.setName('');
     localStorage.setItem("name", '');
   }
+
+  onlyLettersAndNumbers(str : string) {
+    return Boolean(str.match(/^[A-Za-z0-9]*$/));
+  }
+
+  isAlpha(str: string) {
+    return Boolean(str.match("[a-zA-Z]+"));
+}
 }
