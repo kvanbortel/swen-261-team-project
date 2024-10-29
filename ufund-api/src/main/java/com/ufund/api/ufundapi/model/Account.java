bfaf5e5d4fd7df6b6d7e1a91ae8597b2e02c5e7d
rtr8962 <rtr8962@rit.edu>
2024-10-28 21:16:08 -0400

    added fields getters and mutators for moneyFunded and needsFunded

--- ufund-api/src/main/java/com/ufund/api/ufundapi/model/Account.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/model/Account.java
@@ -20,6 +20,41 @@ public class Account {
     //serializable password hash
     @JsonProperty String passwordHash;
 
+    /// THIS WILL CAUSE A MERGE CONFLICT!!!
+    // REMOVE BEFORE MERGE
+    // amount spend on funding needs
+    @JsonProperty double moneyFunded = 0;
+    // number of needs funded
+    @JsonProperty int needsFunded = 0;
+
+    /**
+     * Add an amount to money funded
+     * @param amount
+     * @return the new value for this.moneyFunded
+     */
+    double addMoneyFunded(double amount) {
+        moneyFunded += amount;
+        return moneyFunded;
+    }
+
+    /**
+     * Add a number of needs to needsFunded
+     * @param quantity
+     * @return the new value for this.needsFunded
+     */
+    int addNeedsFunded(int quantity) {
+        needsFunded += quantity;
+        return needsFunded;
+    }
+
+    double getMoneyFunded() {
+        return moneyFunded;
+    }
+
+    int getNeedsFunded() {
+        return needsFunded;
+    }
+
     /**
      * Constructs an account object using a given name and a given Basket
      * @param name a name for the account object
