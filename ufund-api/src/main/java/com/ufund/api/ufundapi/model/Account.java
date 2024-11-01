5ec685003c81d967235e0235facdbcc80862fdb4
rtr8962 <rtr8962@rit.edu>
2024-11-01 14:53:34 -0400

    intermediate commit - fixing getRank

--- ufund-api/src/main/java/com/ufund/api/ufundapi/model/Account.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/model/Account.java
@@ -1,5 +1,6 @@
 package com.ufund.api.ufundapi.model;
 
+import java.util.List;
 import java.util.logging.Logger;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -144,6 +145,26 @@ public class Account implements Comparable<Account> {
         if (this.getName().compareTo(other.getName()) > 0) { return 1; }
         // only possible if they are the same account
         return 0;
+    }
+
+    /**
+     * Gets a users rank given an alread-sorted list of accounts. 
+     * This allows us to avoid sorting a list many times
+     *  (ie get the rank for 5 different users)
+     * Accounts are also not aware of other accounts and therefore cannot
+     *  sort themselves without the outside information of a list of Accounts
+     * 
+     * @param sortedAccounts a list of accounts sorted by rank
+     */
+    public int getRank(List<Account> accounts) {
+        int rank = 1; // minimum possible rank
+        for (Account account : accounts) {
+            // increment the rank for every user that is a lower (better) rank
+            System.out.println(this.toString() + ".compareTo(" + account.toString() + ")");
+            if (this.compareTo(account) == 1);
+                rank++;
+        }
+        return rank;
     }
 
     @Override
