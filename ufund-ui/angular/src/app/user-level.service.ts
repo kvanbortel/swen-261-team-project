5897c0169e7ffedcf073deb9d735be0d024588cc
kjv7359 <kjv7359@rit.edu>
2024-11-10 14:58:32 -0500

    fix userlevel to be consecutive

--- ufund-ui/angular/src/app/user-level.service.ts
+++ ufund-ui/angular/src/app/user-level.service.ts
@@ -36,14 +36,14 @@ export class UserLevelService {
 
     const topPercentIndex = Math.ceil(sortedAccounts.length * -this.PERCENT_INDEX) - 1;
 
-    if (moneyFunded === maxDonation) {
-      userLevel = UserLevel.CHAMPION
-    }
-    else if (sortedAccounts.indexOf(account) <= topPercentIndex) {
-      userLevel = UserLevel.MASTER;
-    }
-    else if (moneyFunded >= this.PRO_MIN_FUNDED) {
+    if (moneyFunded >= this.PRO_MIN_FUNDED) {
       userLevel = UserLevel.PRO;
+      if (sortedAccounts.indexOf(account) <= topPercentIndex) {
+        userLevel = UserLevel.MASTER;
+        if (moneyFunded === maxDonation) {
+          userLevel = UserLevel.CHAMPION;
+        }
+      }
     }
 
     return userLevel;
